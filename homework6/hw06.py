6e301b0d675ad5a258fc2cfbc34a3436fb66d7e7
Applee-Chen <applejingruchen@icloud.com>
2025-11-16 01:19:20 +0000

    homework6

--- homework6/hw06.py
+++ homework6/hw06.py
@@ -153,14 +153,13 @@ def deep_map_mut(func, s):
     <9 <16> 25 36>
     """
     if s is Link.empty:
-        return None
-    if isinstance(s.first, Link) is True:
+        return
+    if isinstance(s.first, Link):
         deep_map_mut(func, s.first)
-    elif isinstance(s.first, Link) is False:
+    else:
         s.first = func(s.first)
     deep_map_mut(func, s.rest)
     
-
 
 
 def two_list(vals, counts):
