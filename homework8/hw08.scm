7d57769462f210f8c40a2f031c9d7b36f5937de0
Applee-Chen <applejingruchen@icloud.com>
2025-11-23 16:02:58 +0000

    homework8 finished

--- homework8/hw08.scm
+++ homework8/hw08.scm
@@ -1,7 +1,23 @@
-(define (ascending? s) 'YOUR-CODE-HERE)
+(define (ascending? s)
+  (if (or (null? s) (null? (cdr s)))
+      #t
+      (and (<= (car s) (car (cdr s))) (ascending? (cdr s)))))
 
-(define (my-filter pred s) 'YOUR-CODE-HERE)
 
-(define (interleave lst1 lst2) 'YOUR-CODE-HERE)
+(define (my-filter pred s) 
+    (if (null? s) nil (if (pred (car s)) (append (list(car s)) (my-filter pred (cdr s))) (my-filter pred (cdr s)))))
 
-(define (no-repeats s) 'YOUR-CODE-HERE)
+(define (interleave lst1 lst2) 
+  (if (null? lst1)
+      lst2 
+      (if (null? lst2)
+          lst1
+          (append (list (car lst1))
+                  (interleave lst2 (cdr lst1)))))) 
+
+
+(define (no-repeats s)
+    (if (null? s) 
+        nil 
+        (cons (car s) 
+            (filter (lambda (x) (not (= x (car s)))) (no-repeats (cdr s))))))
